88b8e806a5d1d84600ddc4514f5c5d25efdff1eb
Subhanjan Adhikary <subhanjan.adhikary@codelogicx.com>
2022-09-12 02:21:37 +0530

    global title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     ** Headers of the page
     */
     head: {
-        title: 'Neveena | My resume website' || process.env.npm_package_name,
+        title: 'Subhanjan | Portfolio' || process.env.npm_package_name,
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
